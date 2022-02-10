3470d31df344421907a7b3c0953c945d74bdd57c
katherin <ft.katheochoa@gmail.com>
2022-02-09 19:18:46 -0500

    added Footer

--- react-projectEcommerce/client/Notes.md
+++ react-projectEcommerce/client/Notes.md
@@ -3,3 +3,11 @@
 1. npm install -D tailwindcss postcss autoprefixer
 2. npx tailwindcss init
 3. add the paths with = "./src/\*_/_.{js,jsx,ts,tsx}",
+4. FontAwesome :
+
+```
+npm i --save @fortawesome/fontawesome-svg-core
+npm install --save @fortawesome/free-solid-svg-icons
+npm install --save @fortawesome/react-fontawesome
+
+```
--- react-projectEcommerce/client/src/components/Footer/Footer.jsx
+++ react-projectEcommerce/client/src/components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
   return (
-    <div className="w-auto bg-indigo-600 h-24">
-      Copyright 2022 Variedades o&f. All rights reserved.| Handcrafted by
-      Katherin Ochoa
+    <div className="w-auto bg-indigo-600 h-24 flex items-center justify-center">
+      <span className="text-white">
+        <FontAwesomeIcon icon={faCopyright} className="mr-1" />
+        Copyright 2022 <b>Variedades o&f.</b> All rights reserved. | Handcrafted
+        by Katherin Ochoa
+      </span>
     </div>
   );
 }
