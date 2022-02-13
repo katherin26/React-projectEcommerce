184f63ce3d6ce8da5c9e8bd0505db333e777cb65
katherin <ft.katheochoa@gmail.com>
2022-02-13 02:12:30 -0500

    added logos and links in footer component

--- react-projectEcommerce/client/src/components/Footer/Footer.jsx
+++ react-projectEcommerce/client/src/components/Footer/Footer.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopyright } from "@fortawesome/free-solid-svg-icons";
-import Juegosw from "../../img/juegosw.png";
+
 import FooterBtn from "./FooterBtn";
+import Juegosw from "../../img/wplay2.png";
+import Davivienda from "../../img/davivienda.png";
+import Megared from "../../img/megared.png";
+import Nequi from "../../img/nequi2.png";
+import Puntored from "../../img/puntored.png";
 
 let companyType = [
   { image: Juegosw, href: "https://apuestas.wplay.co/es" },
-  { image: "", href: "" },
-  { image: "", href: "" },
-  { image: "", href: "" },
-  { image: "", href: "" },
+  {
+    image: Davivienda,
+    href: "https://www.davivienda.com/wps/portal/personas/nuevo",
+  },
+  { image: Megared, href: "https://www.megared.co/" },
+  { image: Nequi, href: "https://www.nequi.com.co/" },
+  { image: Puntored, href: "https://puntored.co/" },
 ];
 
 function Footer() {
   return (
-    <div className="w-auto bg-sky-900 h-24 flex items-center justify-center lg:flex-col">
+    <div className="w-auto bg-sky-900 h-28 flex items-center justify-center flex-col text-center">
       <div className="flex flex-row">
         {companyType.map((element) => (
           <FooterBtn image={element.image} href={element.href} />
         ))}
       </div>
-      <div>
+      <div className="mt-5">
         <span className="text-white  text-sm lg:text-2xl text-center">
           <FontAwesomeIcon icon={faCopyright} className="mr-1" />
           Copyright 2022 <b>Variedades OYF.</b> All rights reserved. |
--- react-projectEcommerce/client/src/components/Footer/FooterBtn.jsx
+++ react-projectEcommerce/client/src/components/Footer/FooterBtn.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 function FooterBtn({ image, href }) {
   return (
-    <div className=" ">
+    <div className="ml-5  ">
       <a
         href={href}
-        className="w-full flex items-center justify-center  border border-transparent  rounded-md text-sky-700 bg-sky-100  shadow-lg shadow-sky-400/50 hover:bg-sky-200  md:py-2  md:px-5 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:transform-none"
+        className=" w-full flex items-center justify-center  border border-transparent  
+        rounded-md text-sky-700 bg-sky-100  shadow-lg shadow-sky-400/30 hover:bg-sky-200  
+        md:py-1  md:px-9  transition transform hover:-translate-y-1 motion-reduce:transition-none 
+        motion-reduce:transform-none"
       >
-        <img src={image} alt="image" className=" w-10 h-8 " />
+        <img src={image} alt="image" className=" w-10 h-5 object-contain " />
       </a>
     </div>
   );
